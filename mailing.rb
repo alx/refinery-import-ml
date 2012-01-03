d351060be321dd1f8aa88509ca92015292d8319b
Alexandre Girard <git@alexgirard.com>
2012-01-03 15:30:42 +0100

    correct markdown conversion and hide local production.sqlite3

--- mailing.rb
+++ mailing.rb
@@ -2,7 +2,9 @@ require 'rubygems'
 require 'yaml'
 require 'gmail'
 require 'sqlite3'
-require 'kramdown'
+require 'redcarpet'
+require 'htmlentities'
+require 'iconv'
 
 @config = YAML.load_file("config.yml")
 @mail_account = @config['mailing']
@@ -28,10 +30,20 @@ end
 
 def insert_email_as_post(email_index)
   if email = @gmail.inbox.emails(:unread, :to => "tetalab@lists.tetalab.org")[email_index]
+
+    coder = HTMLEntities.new
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
+
     title = email.subject.gsub("[Tetalab] ", "")
+
     content = content_from_multipart_mail(email) if email.multipart?
-    content = Kramdown::Document.new(content).to_html
-    @db.execute("INSERT INTO blog_posts (title, body, draft, published_at, created_at) VALUES (?, ?, 'true', ?, ?)", [title, content, Time.now.to_s, Time.now.to_s])
+    content = Iconv.iconv('utf-8', 'iso8859-1', content).first
+    content = markdown.render(content)
+    #coder.encode(content, :decimal))
+
+    query = "INSERT INTO blog_posts (title, body, draft, published_at, created_at, updated_at, user_id) VALUES (?, ?, ?, ?, ?, ?, ?)"
+    @db.execute(query, [title, content, 't', Time.now.to_s, Time.now.to_s, Time.now.to_s, 1])
+
     puts "'#{title}' inserted in db"
   else
     puts "no mail found with this index"
@@ -42,8 +54,8 @@ def select_email_to_insert
   index = 0
   puts "[0] - mark all as read"
   @gmail.inbox.emails(:unread, :to => "tetalab@lists.tetalab.org").each do |email|
-    puts "[#{index}] - #{email.subject}"
     index += 1
+    puts "[#{index}] - #{email.subject}"
     email.mark :unread
   end
   puts "---"
@@ -51,17 +63,13 @@ def select_email_to_insert
 end
 
 def mark_all_read
-  @gmail.inbox.emails(:unread, :to => "tetalab@lists.tetalab.org")..each{|email| email.mark :read}
+  @gmail.inbox.emails(:unread, :to => "tetalab@lists.tetalab.org").each{|email| email.mark :read}
   puts "All mails marked as read"
 end
 
 # Console interface
 if ARGV[0] && index = ARGV[0].to_i
-  if index == 0
-    mark_all_read
-  else
-    insert_email_as_post index
-  end
+  index == 0 ? mark_all_read : insert_email_as_post(index - 1)
 else
   select_email_to_insert
 end
